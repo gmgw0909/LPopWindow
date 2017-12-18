3cec7b6fc5b77b5c975f68ac74578f0de98bfeef
gmgw0909 <1003220931@qq.com>
2017-12-18 10:41:13 +0800

    保证app可以运行

---
+++ app/src/main/java/com/leeboo/lpopwindow/MainActivity.java
@@ -0,0 +1,11 @@
+package com.leeboo.lpopwindow;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 创建者 LeeBoo
+ * 创建时间 2017/12/18
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
